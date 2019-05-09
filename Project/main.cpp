e7ff76277ea0ee79cca715721634c12833a31aee
Lilia <lilia.manukyan@hotmail.com>
2019-05-09 16:56:35 +0400

    promoting T to int type

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
+template <typename T>
+auto promoteToPrintableIntegerType(T i) -> decltype(+i)
+{
+	return +i;
+}
+
 int main()
 {
 	unsigned char age = 65;
 
-	// prints garbage
-	std::cout << age << '\n';
-	std::cout << &age << '\n';
-
-	// cast to a type that compiler doesn't interpret as characters
-	std::cout << static_cast<unsigned>(age) << '\n';
-	std::cout << static_cast<const void *>(&age) << '\n';
+	std::cout << promoteToPrintableIntegerType(age) << '\n';
 
 	std::cin.get();
 	return 0;
