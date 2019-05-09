8af202d41ed3832546bd28dfb5c93708602fb5e2
Lilia <lilia.manukyan@hotmail.com>
2019-05-09 12:46:26 +0400

    Printing for entire class hierarchy

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,14 +1,45 @@
 #include <iostream>
 
-struct Fred
+class Base
 {
-	int a;
+	friend std::ostream &operator<<(std::ostream &o, const Base &b);
+protected:
+	virtual void printOn(std::ostream &s) const;
+private:
+	std::string m_type = "base";
 };
+
+inline std::ostream &operator<<(std::ostream &o, const Base &b)
+{
+	b.printOn(o);
+	return o;
+}
+
+void Base::printOn(std::ostream &s) const
+{
+	s << m_type << '\n';
+	s << std::string(4, ' ');
+}
+
+class Derived : public Base
+{
+protected:
+	void printOn(std::ostream &s) const override;
+private:
+	std::string m_type = "derived";
+};
+
+void Derived::printOn(std::ostream &s) const
+{
+	Base::printOn(s);
+	s << m_type << '\n';
+	s << std::string(4, ' ');
+}
 
 int main()
 {
-	Fred x;
-	std::cout << x.a << std::endl;
+	Derived d;
+	std::cout << d;
 
 	std::cin.get();
 	return 0;
