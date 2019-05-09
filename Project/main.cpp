b31295b39e877bf9880cbd024f312cedc3f42d89
Lilia <lilia.manukyan@hotmail.com>
2019-05-09 22:36:23 +0400

    reverse what's in parentheses

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
+#include <stack>
 
-template <typename T>
-auto promoteToPrintableIntegerType(T i) -> decltype(+i)
+std::string reverseInParentheses(std::string s)
 {
-	return +i;
+	std::stack<int> st;
+	for (int i = 0; i < s.size(); ++i)
+	{
+		if (s[i] == '(')
+			st.push(i);
+		if (s[i] == ')')
+		{
+			int start = st.top(); st.pop();
+			reverse(s.begin() + start + 1, s.begin() + i);
+		}
+	}
+	std::string ans;
+	for (char i : s)
+		if (i != '(' && i != ')')
+			ans = ans + i;
+	return ans;
 }
+
 
 int main()
 {
-	unsigned char age = 65;
-
-	std::cout << promoteToPrintableIntegerType(age) << '\n';
+	std::cout << reverseInParentheses("foo(bar(baz))blim");
 
 	std::cin.get();
 	return 0;
