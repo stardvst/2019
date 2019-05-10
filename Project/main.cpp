591f1520db6b3914541439176e18f051f97c2007
Lilia <lilia.manukyan@hotmail.com>
2019-05-10 21:17:37 +0400

    multiple inheritance: ctor calls

--- Project/main.cpp
+++ Project/main.cpp
@@ -3,36 +3,46 @@
 class Top
 {
 public:
+	Top() {}
+	Top(int t) : top(t) {}
+	virtual ~Top() {}
 	int top = 1;
 };
 
 class Left : virtual public Top
 {
 public:
+	Left() {}
+	Left(int t, int l) : Top(t), left(l) {}
 	int left = 2;
 };
 
 class Right : virtual public Top
 {
 public:
-	int right = 2;
+	Right() {}
+	Right(int t, int r) : Top(t), right(r) {}
+	int right = 3;
 };
 
 class Bottom : public Left, public Right
 {
 public:
-	int bottom = 3;
+	Bottom() {}
+	//Bottom(int t, int l, int r, int b) : Left(t, l), Right(t, r), bottom(b) {}
+	Bottom(int t, int l, int r, int b) : Top(t), Left(t, l), Right(t, r), bottom(b) {}
+	int bottom = 4;
 };
 
 int main()
 {
-	auto bottom = new Bottom;
-	Left *left = bottom;
-	Right *right = bottom;
+	Bottom bottom(1000, 2, 3, 4);
+	std::cout << bottom.Left::top << ' ' << bottom.Right::top << ' '
+		<< bottom.left << ' ' << bottom.right << ' ' << bottom.bottom << '\n';
 
-	std::cout << "bottom: " << bottom << '\n';
-	std::cout << "left: " << left << ", left->left: " << left->left << ", left->top: " << left->top << '\n';
-	std::cout << "right: " << right << ", right->right: " << left->left << ", right->top: " << left->top << '\n';
+	// bottom.Left::top and bottom.Right::top aren't 1000
+	// because Top() is called before Left(int, int) and Right(int, int)
+	// from Bottom() => need to call Top(int) from Bottom()
 
 	std::cin.get();
 	return 0;
