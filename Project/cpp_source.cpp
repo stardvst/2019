82c2e08d0d12cb5be818367027fae3419f630331
Lilia <lilia.manukyan@hotmail.com>
2019-08-18 08:35:45 +0400

    sfinae (simple)

--- Project/cpp_source.cpp
+++ Project/cpp_source.cpp
@@ -1,111 +1,20 @@
 #include <iostream>
-#include <vector>
-#include <unordered_map>
 
-struct Person;
-struct Animal;
-struct Cat;
-struct Dog;
-class ReactionVisitor;
-
-struct AnimalVisitor
+struct Test
 {
-	virtual void visitAnimal(Cat *animal) = 0;
-	virtual void visitAnimal(Dog *animal) = 0;
+	using foo = int;
 };
 
-struct Animal
-{
-	virtual std::string noise() const = 0;
-	virtual void visit(ReactionVisitor &visitor) = 0;
-	virtual ~Animal() = default;
-};
+template <typename T>
+void f(typename T::foo) {}
 
-struct Cat : Animal
-{
-	virtual std::string noise() const override
-	{
-		return "meow";
-	}
-
-	void visit(ReactionVisitor &visitor);
-};
-
-struct Dog : Animal
-{
-	virtual std::string noise() const override
-	{
-		return "woof";
-	}
-
-	void visit(ReactionVisitor &visitor) override;
-
-	enum Breed { Dashchund, Labrador };
-	Breed m_breed = Dashchund;
-};
-
-struct ReactionVisitor : AnimalVisitor
-{
-	ReactionVisitor(Person *person) : m_person(person) {}
-
-	void visitAnimal(Cat *cat) override;
-	void visitAnimal(Dog *dog) override;
-
-	Person *m_person;
-};
-
-struct Person
-{
-	friend ReactionVisitor;
-
-	void ReactTo(Animal *animal)
-	{
-		ReactionVisitor visitor(this);
-		animal->visit(visitor);
-	}
-
-	void pet(Animal *a)
-	{
-		std::cout << "petting" << std::endl;
-	}
-
-	void runaway(Animal *a)
-	{
-		std::cout << "run away" << std::endl;
-	}
-};
-
-void Cat::visit(ReactionVisitor &visitor)
-{
-	visitor.visitAnimal(this);
-}
-
-void Dog::visit(ReactionVisitor &visitor)
-{
-	visitor.visitAnimal(this);
-}
-
-void ReactionVisitor::visitAnimal(Cat *cat)
-{
-	m_person->pet(cat);
-}
-
-void ReactionVisitor::visitAnimal(Dog *dog)
-{
-	if (dog->m_breed == Dog::Dashchund)
-		m_person->runaway(dog);
-	else
-		m_person->pet(dog);
-}
+template <typename T>
+void f(T) {}
 
 int main()
 {
-	auto dog = new Dog;
-	auto cat = new Cat;
-
-	Person p;
-	p.ReactTo(dog);
-	p.ReactTo(cat);
+	f<Test>(10);
+	f<int>(10);
 
 	std::cin.get();
 	return 0;
