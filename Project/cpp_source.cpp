bed823902c985c3ebfcce87f49514cfad1e305ab
Lilia <lilia.manukyan@hotmail.com>
2019-08-22 08:55:48 +0400

    recursive sum

--- Project/cpp_source.cpp
+++ Project/cpp_source.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <limits>
 
+template<typename T>
+T sum(T arg)
+{
+	return arg;
+}
+
+template<typename T, typename ...Args>
+T sum(T arg, Args... args)
+{
+	return arg + sum<T>(args...);
+}
+
 int main()
 {
-	int n = 3;
-	int i = 0;
-
-	switch (n % 2)
-	{
-		case 0:
-			do
-			{
-				++i;
-		case 1: ++i;
-			}
-			while (--n > 0);
-	}
-
-	std::cout << i;
+	auto n1 = sum(0.5, 1, 0.5, 1);
+	auto n2 = sum(1, 0.5, 1, 0.5);
+	std::cout << n1 << n2;
 	
 	std::cin.get();
 	return 0;
