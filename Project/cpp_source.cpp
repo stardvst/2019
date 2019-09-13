6bada67c62a7f772fed56a521805c56dce717c90
Lilia <lilia.manukyan@hotmail.com>
2019-09-13 22:09:02 +0400

    dep injection using interfaces

--- Project/cpp_source.cpp
+++ Project/cpp_source.cpp
@@ -1,29 +1,55 @@
 #include <iostream>
 
-class V8Engine
+struct IEngine
+{
+	virtual void start() = 0;
+	virtual void stop() = 0;
+	virtual ~IEngine() = default;
+};
+
+std::unique_ptr<IEngine> makeV8Engine();
+
+class V8Engine : public IEngine
 {
 public:
-	void start() { std::cout << "V8Engine::start\n"; }
-	void stop() { std::cout << "V8Engine::stop\n"; }
+	void start() override { std::cout << "V8Engine::start\n"; }
+	void stop() override { std::cout << "V8Engine::stop\n"; }
 };
+
+std::unique_ptr<IEngine> makeV8Engine()
+{
+	return std::make_unique<V8Engine>();
+}
 
 class Car
 {
 public:
+	Car(std::unique_ptr<IEngine> &&engine)
+		: m_engine(std::move(engine))
+	{
+	}
+
 	void drive()
 	{
-		m_engine.start();
+		m_engine->start();
 		std::cout << "driving...\n";
-		m_engine.stop();
+		m_engine->stop();
 	}
 
 private:
-	V8Engine m_engine;
+	std::unique_ptr<IEngine> m_engine;
+};
+
+class MockEngine : public IEngine
+{
+public:
+	void start() override { std::cout << "MockEngine::start\n";	}
+	void stop() override { std::cout << "MockEngine::stop\n"; }
 };
 
 int main()
 {
-	Car car;
+	Car car(std::make_unique<MockEngine>());
 	car.drive();
 
 	std::cin.get();
