1fbb903f30abdc6de9cdf683357d050178b42b63
Lilia <lilia.manukyan@hotmail.com>
2019-09-14 10:51:55 +0400

    shared_ptr + weak_ptr

--- Project/cpp_source.cpp
+++ Project/cpp_source.cpp
@@ -1,43 +1,14 @@
 #include <iostream>
-#include <boost/intrusive_ptr.hpp>
-
-struct Person
-{
-	std::string name = "Empty name";
-	int age;
-
-	// intrusive ptr
-	long references = 0;
-};
-
-// must provide these functions
-inline void intrusive_ptr_add_ref(Person *person)
-{
-	++person->references;
-}
-
-inline void intrusive_ptr_release(Person *person)
-{
-	if (--person->references == 0)
-		delete person;
-}
-
-void test()
-{
-	//std::shared_ptr<Person> person(new Person);
-
-	auto pPerson = new Person;
-
-	// same object can be "shared", unlike shared_ptr
-	boost::intrusive_ptr<Person> person1(pPerson);
-	boost::intrusive_ptr<Person> person2(pPerson);
-	std::cout << person1->name << '\n';
-	std::cout << person2->name << '\n';
-}
 
 int main()
 {
-	test();
+	std::shared_ptr<int> int1 = std::make_shared<int>(100);
+	std::weak_ptr<int> weak1(int1);
+	const std::weak_ptr<int>& weak2(weak1);
+
+	std::shared_ptr<int> orig = weak2.lock();
+
+	std::cout << *orig << '\n';
 
 	std::cin.get();
 	return 0;
