3b4331427ebe3c60e3c156b9271e1a00895e4f20
Lilia <lilia.manukyan@hotmail.com>
2019-09-27 21:29:48 +0400

    malloc + free

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -4,32 +4,15 @@ struct S
 {
 	S() { std::cout << "make an S\n"; }
 	~S() { std::cout << "destroy an S\n"; }
-	S(const S &) { std::cout << "copy init an S\n"; }
-	S &operator=(const S &) { std::cout << "copy assign an S\n"; }
 };
-
-S *getRawS()
-{
-	return new S;
-}
-
-std::unique_ptr<S> getUniquePtrS()
-{
-	return std::make_unique<S>();
-}
 
 int main()
 {
-	std::cout << "start main\n";
-	S *rawS = getRawS();
+	auto memory = (S *)malloc(sizeof S);
+	new (memory) S();
 
-	std::cout << "after raw, before smart pointer\n";
-	std::unique_ptr<S> smartS = getUniquePtrS();
-
-	std::cout << "exit main\n";
-
-	// leaks rawS
-	// implicitly deletes smartS
+	memory->~S();
+	free(memory);
 
 	std::cin.get();
 }
