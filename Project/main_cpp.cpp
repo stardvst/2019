b1120f54160e2b1234fdaf893f137b96bf58b4bc
Lilia <lilia.manukyan@hotmail.com>
2019-09-28 13:42:45 +0400

    implement set in terms of another container, and list by default

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 #include <stack>
+#include <list>
+
+template <typename T, typename Container = std::list<T>>
+struct MySet
+{
+public:
+	void push_back(const T &value)
+	{
+		c.push_back(value);
+	}
+
+	std::size_t size() const
+	{
+		return c.size();
+	}
+
+private:
+	Container c;
+};
 
 template <typename T>
 struct MyContainer
@@ -9,14 +28,29 @@ struct MyContainer
 	using const_reference = T;
 	using size_type = int;
 
-	int size() const { return 356; }
+	int size() const { std::cout << __FUNCTION__ << '\n'; return 0; }
+	void push_back(const T &/* value */) { std::cout << __FUNCTION__ << '\n'; }
 };
 
 int main()
 {
-	std::stack<int, MyContainer<int>> stack;
+	std::cout << "MySet<int>\n";
 
-	std::cout << stack.size() << '\n';
+	MySet<int> set1;
+	set1.push_back(3);
+	set1.push_back(3);
+	set1.push_back(3);
+
+	std::cout << set1.size() << '\n';
+
+	std::cout << "\nMySet<int, MyContainer<int>>\n";
+
+	MySet<int, MyContainer<int>> set2;
+	set2.push_back(3);
+	set2.push_back(3);
+	set2.push_back(3);
+
+	std::cout << set2.size() << '\n';
 
 	std::cin.get();
 }
