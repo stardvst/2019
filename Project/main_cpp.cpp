37af1484db4a0cb3cc2ce95c13736cffd61c7315
Lilia <lilia.manukyan@hotmail.com>
2019-09-28 13:33:44 +0400

    implementing stack in terms of custom container

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
+#include <stack>
 
-struct S {};
-struct W : private S { int a; };
-struct T { S s; };
+template <typename T>
+struct MyContainer
+{
+	using value_type = T;
+	using reference = T;
+	using const_reference = T;
+	using size_type = int;
+
+	int size() const { return 356; }
+};
 
 int main()
 {
-	std::cout << sizeof(S) << '\n';
-	std::cout << sizeof(W) << '\n';
-	std::cout << sizeof(T);
+	std::stack<int, MyContainer<int>> stack;
+
+	std::cout << stack.size() << '\n';
 
 	std::cin.get();
 }
