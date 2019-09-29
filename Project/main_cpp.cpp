48cfb266d76fd36527d83937046b13a089a6cdbe
Lilia <lilia.manukyan@hotmail.com>
2019-09-29 19:06:08 +0400

    type of lambda function for <void()> functions

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,56 +1,49 @@
 #include <iostream>
-#include <stack>
-#include <list>
 
-template <typename T, typename Container = std::list<T>>
-struct MySet
+struct FunctionBase
 {
-public:
-	void push_back(const T &value)
-	{
-		c.push_back(value);
-	}
-
-	std::size_t size() const
-	{
-		return c.size();
-	}
-
-private:
-	Container c;
+	virtual void operator()() = 0;
+	virtual ~FunctionBase() = default;
 };
 
-template <typename T>
-struct MyContainer
+template <typename TFunc>
+struct FunctionImpl : FunctionBase
 {
-	using value_type = T;
-	using reference = T;
-	using const_reference = T;
-	using size_type = int;
+	TFunc functor;
 
-	int size() const { std::cout << __FUNCTION__ << '\n'; return 0; }
-	void push_back(const T &/* value */) { std::cout << __FUNCTION__ << '\n'; }
+	FunctionImpl(TFunc f) : functor(f) {}
+
+	void operator()() override
+	{
+		functor();
+	}
 };
+
+struct VoidFunctionCaller
+{
+	std::shared_ptr<FunctionBase> functor;
+
+	template <typename TFunc>
+	VoidFunctionCaller(TFunc f)
+		: functor(std::shared_ptr<FunctionBase>(new FunctionImpl(f)))
+	{
+	}
+
+	void operator()()
+	{
+		(*functor)();
+	}
+};
+
+void callFunction(VoidFunctionCaller f)
+{
+	f();
+}
 
 int main()
 {
-	std::cout << "MySet<int>\n";
-
-	MySet<int> set1;
-	set1.push_back(3);
-	set1.push_back(3);
-	set1.push_back(3);
-
-	std::cout << set1.size() << '\n';
-
-	std::cout << "\nMySet<int, MyContainer<int>>\n";
-
-	MySet<int, MyContainer<int>> set2;
-	set2.push_back(3);
-	set2.push_back(3);
-	set2.push_back(3);
-
-	std::cout << set2.size() << '\n';
+	auto f = []() { std::cout << "from lambda\n"; };
+	callFunction(f);
 
 	std::cin.get();
 }
