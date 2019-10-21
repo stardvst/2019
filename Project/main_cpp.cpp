8b20a1b1eea1d3583f9ee7c006d16f7d53907d06
Lilia <lilia.manukyan@hotmail.com>
2019-10-21 21:21:44 +0400

    std::move() implementation

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,119 +1,42 @@
 #include <iostream>
-#include <limits>
-#include <vector>
 
-namespace MyLib
+template <typename T>
+struct remove_ref
 {
+	using type = T;
+};
 
-	template <typename T>
-	class MyAlloc
-	{
-	public:
-		using value_type = T;
-		using pointer = T *;
-		using const_pointer = const T *;
-		using reference = T &;
-		using const_reference = const T &;
-		using size_type = std::size_t;
-		using difference_type = std::ptrdiff_t;
+template <typename T>
+struct remove_ref<T &>
+{
+	using type = T;
+};
 
-		MyAlloc() noexcept = default;
-		MyAlloc(const MyAlloc &) noexcept = default;
+template <typename T>
+struct remove_ref<T &&>
+{
+	using type = T;
+};
 
-		template <typename U>
-		MyAlloc(const MyAlloc<U> &) noexcept {};
-
-		~MyAlloc() = default;
-
-		template <typename U>
-		struct rebind
-		{
-			using other = MyAlloc<U>;
-		};
-
-		static pointer address(reference value)
-		{
-			return &value;
-		}
-
-		static const_pointer address(const_reference value)
-		{
-			return &value;
-		}
-
-		pointer allocate(size_type num, const void *= nullptr)
-		{
-			std::cerr <<
-				"allocate " << num << " element(s) of size " << sizeof(T) << '\n';
-
-			auto ret = static_cast<pointer>(::operator new(num * sizeof(T)));
-			std::cerr << "allocated at " << static_cast<void *>(ret) << '\n';
-			return ret;
-		}
-
-		static void construct(pointer p, const T &value)
-		{
-			std::cerr << "constructing " << value << "...\n";
-			new(static_cast<void *>(p)) T(value);
-		}
-
-		static void deallocate(pointer p, size_type num)
-		{
-			std::cerr <<
-				"deallocate " << num << " element(s) of size " << sizeof(T)
-				<< " at " << static_cast<void *>(p) << '\n';
-
-			::operator delete(static_cast<void *>(p));
-		}
-
-		static void destroy(pointer p)
-		{
-			std::cerr << "destroying " << *p << "...\n";
-			p->~T();
-		}
-
-		[[nodiscard]] static size_type max_size() noexcept
-		{
-			return std::numeric_limits<std::size_t>::max() / sizeof(T);
-		}
-	};
-
-	template <typename T1, typename T2>
-	bool operator==(const MyAlloc<T1> &t1, const MyAlloc<T2> &t2) noexcept
-	{
-		return true;
-	}
-
-	template <typename T1, typename T2>
-	bool operator!=(const MyAlloc<T1> &t1, const MyAlloc<T2> &t2) noexcept
-	{
-		return false;
-	}
-
+template <typename T>
+typename remove_ref<T>::type &&myMove(T &&o)
+{
+	return static_cast<typename remove_ref<T>::type &&>(o);
 }
 
-int main() noexcept
+struct A
 {
-	std::vector<int, MyLib::MyAlloc<int>> vec;
+	A() {}
+	A(const A &) { std::cout << "copy\n"; }
+	A &operator=(const A &) { std::cout << "assign\n"; return *this; }
+	A(A &&) noexcept { std::cout << "move\n"; }
+};
 
-	std::cout << "insert 42:\n";
-	vec.push_back(42);
-
-	std::cout << "\ninsert 56:\n";
-	vec.push_back(56);
-
-	std::cout << "\ninsert 11:\n";
-	vec.push_back(11);
-
-	std::cout << "\ninsert 22:\n";
-	vec.push_back(22);
-
-	std::cout << "\ninsert 33:\n";
-	vec.push_back(33);
-
-	std::cout << "\ninsert 44:\n";
-	vec.push_back(44);
+int main()
+{
+	A a;
+	//A b = std::move(a);
+	A b = myMove(a);
 
 	std::cin.get();
-	return 0;
 }
