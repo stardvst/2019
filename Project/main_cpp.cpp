a5302e22662b57cfc478c9000b5d2811d009426c
Lilia <lilia.manukyan@hotmail.com>
2019-10-25 22:10:34 +0400

    throwing dtor, throwing during unwinding

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,42 +1,24 @@
 #include <iostream>
 
-template <typename T>
-struct remove_ref
-{
-	using type = T;
-};
-
-template <typename T>
-struct remove_ref<T &>
-{
-	using type = T;
-};
-
-template <typename T>
-struct remove_ref<T &&>
-{
-	using type = T;
-};
-
-template <typename T>
-typename remove_ref<T>::type &&myMove(T &&o)
-{
-	return static_cast<typename remove_ref<T>::type &&>(o);
-}
-
 struct A
 {
-	A() {}
-	A(const A &) { std::cout << "copy\n"; }
-	A &operator=(const A &) { std::cout << "assign\n"; return *this; }
-	A(A &&) noexcept { std::cout << "move\n"; }
+	~A()
+	{
+		throw 42;
+	}
 };
 
 int main()
 {
-	A a;
-	//A b = std::move(a);
-	A b = myMove(a);
+	try
+	{
+		A a;
+		//throw 32;
+	}
+	catch (int a)
+	{
+		std::cout << a << '\n';
+	}
 
 	std::cin.get();
 }
