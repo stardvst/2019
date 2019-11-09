b9b08d94cfc38aabcdf01e2242a13d10b90a8775
Lilia <lilia.manukyan@hotmail.com>
2019-11-09 21:05:04 +0400

    cpp17 auto -> int

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,31 +1,9 @@
 #include <iostream>
-#include <chrono>
-#include <thread>
-#include <functional>
-
-void timer_start(std::function<void(void)> func, unsigned int interval)
-{
-	std::thread([func, interval]()
-	{
-		while (true)
-		{
-			auto x = std::chrono::steady_clock::now() + std::chrono::milliseconds(interval);
-			func();
-			std::this_thread::sleep_until(x);
-		}
-	}).detach();
-}
-
-void do_something()
-{
-	std::cout << "I am doing something" << std::endl;
-}
+#include <typeinfo>
 
 int main()
 {
-	timer_start(do_something, 3000);
-	while (true)
-		;
-
+	auto x{ 1 };
+	std::cout << typeid(x).name() << std::endl;
 	std::cin.get();
 }
