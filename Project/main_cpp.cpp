69de70fb062ff370419e5f59e73032a7f72db588
Lilia <lilia.manukyan@hotmail.com>
2019-11-09 21:03:33 +0400

    cast operation

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,65 +1,24 @@
 #include <iostream>
 
-struct ABC
+class A
 {
-
-	void set(const int data)
+public:
+	A()
 	{
-		std::cout << "set(" << data << ")\n";
-		if (!this)
-		{
-			std::cout << "this is null, can't set\n";
-			print(); // will crash, calls this->print()
-			return;
-		}
-		this->data = data;
+		std::cout << "ctor" << std::endl;
 	}
-
-	void get() const
+	operator unsigned long()
 	{
-		if (!this)
-		{
-			std::cout << "null ptr deref, can't get\n";
-			print(); // will crash, calls this->print()
-			return;
-		}
-
-		print();
+		return 0;
 	}
-
-	void print() const
-	{
-		//std::cout << "data = " << data << "\n";
-		std::cout << "print\n"; // works isnce no access to data member
-	}
-
-	virtual void vprint()
-	{
-		std::cout << "virtual print\n"; // doesn't work since tries to deref vtable
-	}
-
-	int data;
 };
 
 int main()
 {
-
-	ABC abc;
-	abc.set(100);
-	abc.get();
-
-	ABC *p1Abc = &abc;
-	ABC *p2Abc = nullptr;
-
-	p1Abc->set(111);
-	p1Abc->get();
-
-	p2Abc->set(222);
-	p2Abc->get();
-	// using fptr = void (ABC::*)();
-	// fptr f = &ABC::vprint;
-	// (p2Abc->*f)();
-	// //p2Abc->vprint();
+	A a;
+	if (a == 0UL)
+		std::cout << "a == 0UL" << std::endl;
 
 	std::cin.get();
+	return 0;
 }
