b10930ddc1ede2d19f4d1350aade0af81777f39e
Lilia <lilia.manukyan@hotmail.com>
2019-11-15 13:33:46 +0400

    tuple implementation - element type holder without tuple parameter

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,38 +1,60 @@
 #include <iostream>
-#include <vector>
 
-void push(std::vector<int> &v, int value)
+template <typename... Ts>
+struct tuple
 {
-	// will avoid reallocation
-	if (v.capacity() <= v.size())
+};
+
+template <typename T, typename... Ts>
+struct tuple<T, Ts...> : tuple<Ts...>
+{
+	T data;
+
+	tuple(T v, Ts... args)
+		: tuple<Ts...>(args...)
+		, data(v)
 	{
-		std::cout << "inserting " << value << "..., but first, capacity: " << v.capacity() << std::endl;
-		v.reserve(v.size() * 1.1);
-		std::cout << "capacity after resize: " << v.capacity() << std::endl;
 	}
+};
 
-	// will always be the same, contrary to if we used push_back() without reserve()
-	std::cout << "element addresses:\n";
-	for (const auto &a : v)
-		std::cout << &a << '\n';
+template <size_t, typename...>
+struct elem_type;
 
-	std::cout << '\n';
+template <typename T, typename... Ts>
+struct elem_type<0, T, Ts...>
+{
+	using type = T;
+};
 
-	v.push_back(value);
+template <size_t N, typename T, typename... Ts>
+struct elem_type<N, T, Ts...>
+{
+	using type = typename elem_type<N - 1, Ts...>::type;
+};
+
+template <size_t N, typename... Ts>
+typename std::enable_if<N == 0, typename elem_type<0, Ts...>::type>::type &get(tuple<Ts...> &t)
+{
+	return t.data;
+}
+
+template <size_t N, typename T, typename... Ts>
+typename std::enable_if<N != 0, typename elem_type<N, T, Ts...>::type>::type &get(tuple<T, Ts...> &t)
+{
+	tuple<Ts...> &base = t;
+	return get<N - 1>(base);
 }
 
 int main()
 {
-	std::vector<int> v;
+	tuple<double, uint64_t, const char *> t1(12.2, 42, "big");
 
-	push(v, 5);
-	push(v, 3);
-	push(v, 6);
-	push(v, 7);
-	push(v, 1);
-	push(v, 2);
-	push(v, 4);
+	std::cout << "0th element is " << get<0>(t1) << '\n';
+	std::cout << "1st element is " << get<1>(t1) << '\n';
+	std::cout << "2nd element is " << get<2>(t1) << '\n';
+
+	get<1>(t1) = 103;
+	std::cout << "1st element is " << get<1>(t1) << '\n';
 
 	std::cin.get();
-	return 0;
 }
