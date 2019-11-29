b7b8bd82609a408495cc8f3b93f3263d06928f01
Lilia <lilia.manukyan@hotmail.com>
2019-11-29 23:38:04 +0400

    another tuple impl

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,27 +1,62 @@
 #include <iostream>
 
-//////////////////////////////////////////////////////////////////////////
-// Variadic template to make this happen:
-// property<T1, T2, T3, T4>::type == boost::property<T1, T2, boost::property<T3, T4>>
-//
-template <typename Tag, typename Type, typename This>
-struct PropertyBase
+template <typename... T>
+struct Structure
 {
 };
 
-template <typename Tag, typename Type>
-struct Property
+template <typename T, typename... Rest>
+struct Structure<T, Rest...>
 {
-	using type = Property<Tag, Type>;
+	Structure(const T &f, const Rest &... r)
+		: first(f)
+		, rest(r...)
+	{
+	}
+
+	template <size_t idx>
+	decltype(auto) get()
+	{
+		return GetHelper<idx, Structure<T, Rest...>>::get(*this);
+	}
+
+	T first;
+	Structure<Rest...> rest;
 };
 
-template <typename Tag, typename Type, typename... Types>
-struct Property
+template <size_t idx, typename T>
+struct GetHelper
 {
-	using type = Property<Tag, Type, typename Property<Types...>::type>::type;
+};
+
+template <typename T, typename... Rest>
+struct GetHelper<0, Structure<T, Rest...>>
+{
+	static T get(Structure<T, Rest...> &data)
+	{
+		return data.first;
+	}
+};
+
+template <size_t idx, typename T, typename... Rest>
+struct GetHelper<idx, Structure<T, Rest...>>
+{
+	static T get(Structure<T, Rest...> &data)
+	{
+		return GetHelper<idx - 1, Structure<Rest...>>::get(data.rest);
+	}
 };
 
 int main()
 {
+	Structure<int, char, bool> s(5, 'a', false);
+	std::cout << s.first << '\n';
+	std::cout << s.rest.first << '\n';
+	std::cout << s.rest.rest.first << '\n';
+
+	std::cout << s.get<0>() << '\n';
+	std::cout << s.get<1>() << '\n';
+	std::cout << s.get<2>() << '\n';
+
 	std::cin.get();
 }
