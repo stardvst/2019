4bead8e756299a18d4cb4ce76caf2c8bd618879f
Lilia <lilia.manukyan@hotmail.com>
2019-11-29 13:22:18 +0400

    property<T1, T2, T3, T4>::type == boost::property<T1, T2, boost::property<T3, T4>>

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,104 +1,27 @@
 #include <iostream>
 
-template <typename...>
-struct tuple
-{
-};
-
-template <typename T, typename... Ts>
-struct tuple<T, Ts...> : tuple<Ts...>
-{
-	T data;
-
-	tuple(T v, Ts... args)
-		: tuple<Ts...>(args...)
-		, data(v)
-	{
-	}
-};
-
-template <size_t, typename...>
-struct elem_type;
-
-template <typename T, typename... Ts>
-struct elem_type<0, T, Ts...>
-{
-	using type = T;
-};
-
-template <size_t N, typename T, typename... Ts>
-struct elem_type<N, T, Ts...>
-{
-	using type = typename elem_type<N - 1, Ts...>::type;
-};
-
-template <size_t N, typename... Ts>
-typename std::enable_if_t<N == 0, typename elem_type<0, Ts...>::type> &get(tuple<Ts...> &t)
-{
-	return t.data;
-}
-
-template <size_t N, typename T, typename... Ts>
-typename std::enable_if_t<N != 0, typename elem_type<N, T, Ts...>::type> &get(tuple<T, Ts...> &t)
-{
-	tuple<Ts...> &base = t;
-	return get<N - 1>(base);
-}
-
 //////////////////////////////////////////////////////////////////////////
-
-//template <class ElemType, typename T, typename... Ts>
-//typename std::enable_if_t<std::is_same_v<ElemType, T>, ElemType> &get(tuple<T, Ts...> &t)
-//{
-//	static_assert(!(std::is_same_v<T, Ts> || ...), "duplicate type T in get<T>(tuple)"); // c++17
-//	return t.data;
-//}
+// Variadic template to make this happen:
+// property<T1, T2, T3, T4>::type == boost::property<T1, T2, boost::property<T3, T4>>
 //
-//template <class ElemType, typename T, typename... Ts>
-//typename std::enable_if_t<!std::is_same_v<ElemType, T>, ElemType> &get(tuple<T, Ts...> &t)
-//{
-//	tuple<Ts...> &base = t;
-//	return get<ElemType>(base);
-//}
-
-template <class ElemType>
-void get(tuple<> &t)
+template <typename Tag, typename Type, typename This>
+struct PropertyBase
 {
-}
+};
 
-template <class ElemType, typename T, typename... Ts, std::enable_if_t<std::is_same_v<T, ElemType>, int> = 0>
-ElemType &get(tuple<T, Ts...> &t)
+template <typename Tag, typename Type>
+struct Property
 {
-	tuple<Ts...> &base = t;
-	static_assert(std::is_void_v<decltype(get<ElemType>(base))>);
-	return t.data;
-}
+	using type = Property<Tag, Type>;
+};
 
-template <class ElemType, typename T, typename... Ts, std::enable_if_t<!std::is_same_v<T, ElemType>, int> = 0>
-decltype(auto) get(tuple<T, Ts...> &t)
+template <typename Tag, typename Type, typename... Types>
+struct Property
 {
-	tuple<Ts...> &base = t;
-	return get<ElemType>(base);
-}
+	using type = Property<Tag, Type, typename Property<Types...>::type>::type;
+};
 
 int main()
 {
-	tuple<double, int, char> t1(12.2, 42, 'a');
-
-	std::cout << "0th element is " << get<0>(t1) << '\n';
-	std::cout << "1st element is " << get<1>(t1) << '\n';
-	std::cout << "2nd element is " << get<2>(t1) << '\n';
-
-	get<1>(t1) = 103;
-	std::cout << "1st element is " << get<1>(t1) << '\n';
-
-	std::cout << "\ndouble element is " << get<double>(t1) << '\n';
-	std::cout << "int element is " << get<int>(t1) << '\n';
-	std::cout << "char element is " << get<char>(t1) << '\n';
-
-	tuple<double, int, char, double, int> t2(12.2, 42, 'a', 3.3, 4);
-	std::cout << "double element of new tuple is " << get<double>(t2) << '\n';
-	std::cout << "int element of new tuple is " << get<int>(t2) << '\n';
-
 	std::cin.get();
 }
