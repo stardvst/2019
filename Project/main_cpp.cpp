5a2de4009d4cae5b6309e17b740e34dc39f01431
Lilia <lilia.manukyan@hotmail.com>
2019-12-07 22:04:59 +0400

    variadic reverse

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,63 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <list>
-#include <set>
-#include <map>
-#include <array>
 
-template <
-	template <typename, typename> class ContainerType,
-	typename ValueType,
-	typename AllocType
->
-void printContainer(const ContainerType<ValueType, AllocType> &c)
+std::vector<int> reverse(int i)
 {
-	for (const auto &v : c)
-		std::cout << v << ' ';
-	std::cout << '\n';
+	return std::vector<int> {i};
 }
 
-template <
-	template <typename, typename...> class ContainerType,
-	typename ValueType,
-	typename... Args
->
-void printContainerVar(const ContainerType<ValueType, Args...> &c)
+template <typename... Ts>
+std::vector<int> reverse(int i, Ts... ts)
 {
-	for (const auto &v : c)
-		std::cout << v << ' ';
-	std::cout << '\n';
-}
-
-template <typename T, typename U>
-std::ostream &operator<<(std::ostream &out, const std::pair<T, U> &p)
-{
-	out << '[' << p.first << ", " << p.second << ']';
-	return out;
+	auto v = reverse(ts...);
+	v.push_back(i);
+	return v;
 }
 
 int main()
 {
-	std::vector<int> v{ 3, 5, 7, 1 };
-	printContainer(v);
-
-	std::list<int> l{ 3, 5, 7, 1 };
-	printContainer(l);
-
-	std::set<int> s{ 3, 5, 7, 1 };
-	//printContainer(s);
-	printContainerVar(s);
-
-	std::map<int, std::string> m{ {42, "foo"}, {81, "bar"} };
-	//printContainer(m);
-	printContainerVar(m);
-
-	std::string ss{ "abcd" };
-	printContainerVar(ss);
-
-	// doesn't work
-	//std::array<int, 4> a{ 3, 5, 7, 1 };
-	//printContainerVar(a);
+	for (const auto &i : reverse(1, 2, 3, 4))
+		std::cout << i << ' ';
 
 	std::cin.get();
 }
