e98e05c86e324d54b1bca18bb15271e7bc390ef9
Lilia <lilia.manukyan@hotmail.com>
2019-12-07 20:58:08 +0400

    print (almost) any container

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,30 +1,63 @@
 #include <iostream>
-#include <tuple>
-#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <array>
 
-template <typename T>
-T adder(T v)
+template <
+	template <typename, typename> class ContainerType,
+	typename ValueType,
+	typename AllocType
+>
+void printContainer(const ContainerType<ValueType, AllocType> &c)
 {
-	return v;
+	for (const auto &v : c)
+		std::cout << v << ' ';
+	std::cout << '\n';
 }
 
-template <typename T, typename... Args>
-T adder(T first, Args... args)
+template <
+	template <typename, typename...> class ContainerType,
+	typename ValueType,
+	typename... Args
+>
+void printContainerVar(const ContainerType<ValueType, Args...> &c)
 {
-	return first + adder(args...);
+	for (const auto &v : c)
+		std::cout << v << ' ';
+	std::cout << '\n';
 }
 
-template <typename... Args>
-auto foldAdder(Args... args)
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &out, const std::pair<T, U> &p)
 {
-	std::cout << __FUNCSIG__ << '\n';
-	return (args + ...);
+	out << '[' << p.first << ", " << p.second << ']';
+	return out;
 }
 
 int main()
 {
-	std::cout << adder(4, 5, 6) << '\n';
-	std::cout << foldAdder<int>(4, 5, 6) << '\n';
+	std::vector<int> v{ 3, 5, 7, 1 };
+	printContainer(v);
+
+	std::list<int> l{ 3, 5, 7, 1 };
+	printContainer(l);
+
+	std::set<int> s{ 3, 5, 7, 1 };
+	//printContainer(s);
+	printContainerVar(s);
+
+	std::map<int, std::string> m{ {42, "foo"}, {81, "bar"} };
+	//printContainer(m);
+	printContainerVar(m);
+
+	std::string ss{ "abcd" };
+	printContainerVar(ss);
+
+	// doesn't work
+	//std::array<int, 4> a{ 3, 5, 7, 1 };
+	//printContainerVar(a);
 
 	std::cin.get();
 }
