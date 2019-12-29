ae97bf97c88ea74ce79cf312098c5a87ad952c59
Lilia <lilia.manukyan@hotmail.com>
2019-12-29 13:39:38 +0400

    false sharing

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,28 +1,108 @@
+#include <thread>
 #include <iostream>
-#include "rulesEngine.h"
+#include <ctime>
+#include <memory>
+#include <mutex>
+#include <condition_variable>
+
+using std::cout;
+using std::endl;
+
+const double M = 8e8;
+
+//Shared_Work holds the work need to be done part1() and part2() separates the work
+// half-and-half n1 and n2 are data items chunk[64] is the separation attempt.
+class Shared_Work
+{
+public:
+	Shared_Work() :n1(1), n2(1), flag1(0), flag2(0)
+	{
+	}
+
+	void part1()
+	{
+		for (; n1 < M; n1 += n1 % 3);
+		flag1 = 1;
+		cond.notify_one();
+	}
+
+	void part2()
+	{
+		for (; n2 < M; n2 += n2 % 3);
+		flag2 = 1;
+		cond.notify_one();
+	}
+
+	long int result()
+	{
+		std::unique_lock<std::mutex> lk(mt);
+		while (flag1 == 0 || flag2 == 0)
+			cond.wait(lk);
+		return n1 + n2;
+	}
+
+private:
+	std::mutex mt;
+	std::condition_variable cond;
+	long int n1;
+	//char chuck[64]; //cache line seperation
+	long int n2;
+	bool flag1;
+	bool flag2;
+};
+
+// a template class for generating threads with different member functions
+//"action _fptr" will be instantiated by &Shared_Work::part1 and part2.
+template<class T>
+class Do_Work
+{
+public:
+	using action = void (T:: *)();
+	Do_Work(std::shared_ptr<Shared_Work> &x, action y) :_p(x), _fptr(y)
+	{
+	}
+	void operator()()
+	{
+		(*_p.*_fptr)();
+	}
+private:
+	std::shared_ptr<Shared_Work> _p;
+	action _fptr;
+};
+
+// single thread work to do
+void one_thread()
+{
+	long int n1 = 1, n2 = 1;
+	for (; n1 < M; n1 += n1 % 3);
+	for (; n2 < M; n2 += n2 % 3);
+	cout << n1 + n2 << endl;
+}
+
+time_t start, end;
+double diff;
 
 int main()
 {
-	int a, b, c;
-	std::cout << "a: ";
-	std::cin >> a;
+	std::shared_ptr<Shared_Work> p(new Shared_Work);
+	Do_Work<Shared_Work> d1(p, &Shared_Work::part1);
+	Do_Work<Shared_Work> d2(p, &Shared_Work::part2);
 
-	std::cout << "b: ";
-	std::cin >> b;
+	time(&start);
+	std::thread t1(d1);
+	std::thread t2(d2);
+	t1.detach();//release the ownership to C++ runtime library.
+	t2.detach();//release the ownership to C++ runtime library.
+	cout << p->result() << endl;
+	time(&end);
+	diff = difftime(end, start);
+	cout << diff << " seconds elapsed for 2 threads calculation." << endl;
 
-	std::cout << "c: ";
-	std::cin >> c;
-
-	RulesEngine rules;
-
-	rules.If(a > 0);
-	rules.If(b > 0);
-
-	PreventingRulesEngine &neg = rules.Not;
-	neg.If(c == 0);
-	std::cin.ignore();
-
-	std::cout << "rules satisfied: " << rules() << '\n';
+	time(&start);
+	one_thread();
+	time(&end);
+	diff = difftime(end, start);
+	cout << diff << " seconds elapsed for 1 thread calculation." << endl;
 
 	std::cin.get();
 }
