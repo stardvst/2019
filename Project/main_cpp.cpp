fa02624a3843369e9da1458852a70b973f9ff15c
Lilia <lilia.manukyan@hotmail.com>
2019-12-29 22:27:24 +0400

    bulk read from file

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,46 +1,13 @@
-#include <cmath>
-#include <functional>
 #include <iostream>
-#include <map>
-
-double add(double a, double b)
-{
-	return a + b;
-}
-
-struct Sub
-{
-	double operator()(double a, double b)
-	{
-		return a - b;
-	}
-};
-
-double multThree(double a, double b, double c)
-{
-	return a * b * c;
-}
+#include <fstream>
+#include <iterator>
 
 int main()
 {
-	using namespace std::placeholders;
+	std::ifstream ifstr("2048.h");
+	std::string content((std::istreambuf_iterator<char>(ifstr)),
+		std::istreambuf_iterator<char>());
+	std::cout << content << '\n';
 
-	std::map<const char, std::function<double(double, double)>> dispTable{
-		{ '+', add },
-		{ '-', Sub() },
-		{ '*', std::bind(multThree, 1, _1, _2) },
-		{ '/', [](double a, double b)
-			{
-				return a / b;
-			}
-		}
-	};
-
-	std::cout << "3.5 + 4.5 = " << dispTable['+'](3.5, 4.5) << std::endl;
-	std::cout << "3.5 - 4.5 = " << dispTable['-'](3.5, 4.5) << std::endl;
-	std::cout << "3.5 * 4.5 = " << dispTable['*'](3.5, 4.5) << std::endl;
-	std::cout << "3.5 / 4.5 = " << dispTable['/'](3.5, 4.5) << std::endl;
-
-	std::cout << std::endl;
 	std::cin.get();
 }
