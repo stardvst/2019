980d1edbde1651fad115016f0c0ec82b03c6e87b
Lilia <lilia.manukyan@hotmail.com>
2019-12-30 21:42:15 +0400

    deprecated namespace

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 
-template <typename T>
-auto getValue(T t)
+namespace [[deprecated]] old_stuff
 {
-	if constexpr (std::is_pointer_v<T>)
-		return *t;
-	else
-		// must be in else case, otherwise both returns will be instantiated
-		return t;
+
+void legecy()
+{
+}
+
 }
 
 int main()
 {
-	int a = 5, *p = &a;
-	std::cout << getValue(a) << ' ' << getValue(p);
+	old_stuff::legecy();
 
 	std::cin.get();
 }
