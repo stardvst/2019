dc7111ccddfa95be5c9b2c29e8112541cde28f87
Lilia <lilia.manukyan@hotmail.com>
2019-12-30 21:11:38 +0400

    fold expr

--- Project/main_cpp.cpp
+++ Project/main_cpp.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
-#include <utility>
 
-void f(std::pair<int, int>)
+auto sumCpp11()
 {
+	return 0;
+}
+
+template <typename T, typename... Args>
+auto sumCpp11(T v, Args... vs)
+{
+	return v + sumCpp11(vs...);
+}
+
+template <typename ...Args>
+auto sumCpp17(Args ...args)
+{
+	//return (args + ... + 0);
+	return (args + ...);
+}
+
+template <typename ...Args>
+void foldPrint(Args &&...args)
+{
+	(std::cout << ... << std::forward<Args>(args)) << '\n';
 }
 
 int main()
 {
-	f(std::pair(42, 42));
+	std::cout << sumCpp11(1, 2, 3) << '\n';
+	foldPrint(sumCpp17(1, 2, 3));
 
 	std::cin.get();
 }
